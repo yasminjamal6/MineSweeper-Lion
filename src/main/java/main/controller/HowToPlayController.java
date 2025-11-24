f610be3512b78213bfa1d38555f7cbe64e064dd2
yasminjamal6 <jamal.yasmeen2062@gmail.com>
2025-11-24 09:16:01 +0200

    Improve home page UI, fix button alignment and navigation flow, and add How-To-Play page

---
+++ src/main/java/main/controller/HowToPlayController.java
@@ -0,0 +1,35 @@
+package main.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.animation.FadeTransition;
+import javafx.util.Duration;
+
+public class HowToPlayController {
+
+    @FXML
+    private void onCloseHowToPlay(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/view/home-view.fxml")
+            );
+            Parent newRoot = loader.load();
+
+            Scene scene = ((Node) event.getSource()).getScene();
+
+            newRoot.setOpacity(0);
+            scene.setRoot(newRoot);
+
+            FadeTransition ft = new FadeTransition(Duration.millis(250), newRoot);
+            ft.setToValue(1);
+            ft.play();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
